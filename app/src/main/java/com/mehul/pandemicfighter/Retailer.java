48020f60b95bed1d9a54c9a6c99c5992c74ecf01
gargmehul10 <garg.mehul10@gmail.com>
2020-04-24 16:11:32 +0530

    Added some ML kit part

--- app/src/main/java/com/mehul/pandemicfighter/Retailer.java
+++ app/src/main/java/com/mehul/pandemicfighter/Retailer.java
@@ -1,22 +1,51 @@
 package com.mehul.pandemicfighter;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import java.util.List;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class Retailer extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private TextView mTextView;
+//    private Button mTextButton;
+    private Bitmap mSelectedImage;
+    // private GraphicOverlay mGraphicOverlay;
+    private Integer mImageMaxWidth;
+    private Integer mImageMaxHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer);
 
+        mImageView = findViewById(R.id.image_view);
+        mTextView = findViewById(R.id.text1);
     }
 
     @Override
@@ -66,4 +95,160 @@ public class Retailer extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 1888);
+            }
+            else
+            {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == 1888 && resultCode == Activity.RESULT_OK)
+        {
+            mSelectedImage = (Bitmap) data.getExtras().get("data");
+
+            if (mSelectedImage != null) {
+                // Get the dimensions of the View
+                Pair<Integer, Integer> targetedSize = getTargetedWidthHeight();
+
+                int targetWidth = targetedSize.first;
+                int maxHeight = targetedSize.second;
+
+                // Determine how much to scale down the image
+                float scaleFactor =
+                        Math.max(
+                                (float) mSelectedImage.getWidth() / (float) targetWidth,
+                                (float) mSelectedImage.getHeight() / (float) maxHeight);
+
+                Bitmap resizedBitmap =
+                        Bitmap.createScaledBitmap(
+                                mSelectedImage,
+                                (int) (mSelectedImage.getWidth() / scaleFactor),
+                                (int) (mSelectedImage.getHeight() / scaleFactor),
+                                true);
+
+                 mImageView.setImageBitmap(resizedBitmap);
+                mSelectedImage = resizedBitmap;
+            }
+
+            // imageView.setImageBitmap(photo);
+            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+            FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                    .getOnDeviceTextRecognizer();
+            // mTextButton.setEnabled(false);
+            recognizer.processImage(image)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<FirebaseVisionText>() {
+                                @Override
+                                public void onSuccess(FirebaseVisionText texts) {
+                                    // mTextButton.setEnabled(true);
+                                    processTextRecognitionResult(texts);
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Task failed with an exception
+                                    // mTextButton.setEnabled(true);
+                                    e.printStackTrace();
+                                }
+                            });
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void CameraButton(View view) {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+        }
+        else
+        {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, 1888);
+        }
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        String k1="";
+        // Replace with code from the codelab to process the text recognition result.
+        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0) {
+            showToast("No text found");
+            return;
+        }
+        // mGraphicOverlay.clear();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    // Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
+                    k1+=elements.get(k).getText();
+                    // mGraphicOverlay.add(textGraphic);
+                }
+            }
+        }
+        mTextView.setText(k1);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    // Returns max image width, always for portrait mode. Caller needs to swap width / height for
+    // landscape mode.
+    private Integer getImageMaxWidth() {
+        if (mImageMaxWidth == null) {
+            // Calculate the max width in portrait mode. This is done lazily since we need to
+            // wait for
+            // a UI layout pass to get the right values. So delay it to first time image
+            // rendering time.
+            mImageMaxWidth = mImageView.getWidth();
+        }
+
+        return mImageMaxWidth;
+    }
+
+    // Returns max image height, always for portrait mode. Caller needs to swap width / height for
+    // landscape mode.
+    private Integer getImageMaxHeight() {
+        if (mImageMaxHeight == null) {
+            // Calculate the max width in portrait mode. This is done lazily since we need to
+            // wait for
+            // a UI layout pass to get the right values. So delay it to first time image
+            // rendering time.
+            mImageMaxHeight =
+                    mImageView.getHeight();
+        }
+
+        return mImageMaxHeight;
+    }
+
+    // Gets the targeted width / height.
+    private Pair<Integer, Integer> getTargetedWidthHeight() {
+        int targetWidth;
+        int targetHeight;
+        int maxWidthForPortraitMode = getImageMaxWidth();
+        int maxHeightForPortraitMode = getImageMaxHeight();
+        targetWidth = maxWidthForPortraitMode;
+        targetHeight = maxHeightForPortraitMode;
+        return new Pair<>(targetWidth, targetHeight);
+    }
 }
